699bcf400b01261a6acf3839e52d3e832b281808
R42 <ranjanrnj44@gmail.com>
2020-08-24 21:05:00 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@
 let heading  = document.querySelector(`h1`);
 heading.textContent = ` `;
 let usr_btn = document.querySelector(`.change_user`);
+
+//some public elements for access
+
 //DOMContentLoaded
 window.addEventListener(`DOMContentLoaded` , prompt_me);
 
@@ -10,6 +13,10 @@ function prompt_me(){
     let username = prompt(`Hi there, Enter your name`);
     if((!username) || (username === null)){
         alert(`Username is necessary`);
+        prompt_me();
+    }
+    else if(username.length >= 20){
+        alert(`Please give only less than 20 characters`);
         prompt_me();
     }
     else{
@@ -43,10 +50,19 @@ my_img.addEventListener(`click` , function(){
 //create dynamic_para
 function dynam(){
     let list = document.querySelector(`nav ul`);
-    let para = document.createElement(`p`);
-    para.textContent = `I am Dynamic`;
-    list.appendChild(para);
+    let liele = document.createElement(`li`);
+    liele.textContent = `I am Dynamic`;
+    list.appendChild(liele);
 }
+
+
+//remove para
+function rmvdynam(){
+    let list = document.querySelector(`nav ul`);
+    list.textContent = ` `;
+}
+let rmvbtn = document.querySelector(".rmvdynamic_para");
+rmvbtn.addEventListener("click" , rmvdynam);
 
 let dyn_btn = document.querySelector(`.dynamic_para`);
 dyn_btn.addEventListener(`click` , dynam);
